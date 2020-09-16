19dc04e1accea138ec19f22120a6932891bfbe77
FlorianRossignol <rossignol.florian.fr@gmail.com>
2020-09-16 15:48:12 +0200

    projet attaque vaisseau

--- main.cpp
+++ main.cpp
@@ -11,9 +11,15 @@ int main ()
 		std::cout << "perdu le vaisseau avait plus de pv \n\n";
 		return 0;
 	}
-	if (pv >2)
+	if (pv >2 && pv ==4)
+
 	{
 		std::cout << "gagn\x82\ le vaisseau est battu " "\n\n";
+	}
+	if (pv >4)
+	{
+		std::cout << "on ne peux pas tricher !\n\n";
+		return 0;
 	}
 	std::cout << "un deuxieme vaisseau est en approche le combattez-vous ?(oui = 1 ou non = 2)\n\n";
 	int questions;
@@ -27,19 +33,28 @@ int main ()
 		std::cout << "vous etes faible!\n\n";
 		return 0;
 	}
+	if (questions >2)
+	{
+		std::cout << "vous etes un peu con\n\n";
+		return 0;
+	}
 	std::cout << "le vaisseau ennemie est sur le point d'attaquer combiens de degats lui inflige vous ? (maximum 8)\n\n";
 	int pv2;
 	pv2 = 7;
 	std::cin >> pv2,"\n\n";
-	if (pv2 <= 7)
+	if (pv2 <= 8)
 	{
 		std::cout << "bravo vous devenez de plus en plus fort !\n\n";
 	}
-		if (pv2 >7)
+		if (pv2 >8 && pv2 ==8)
 		{
 			std::cout << "dommage, on recommence ?\n\n";
 			return 0;
 		}
-	
+	if (pv2>8)
+	{
+		std::cout << "je peux plus rien pour vous maintenant\n\n";
+		return 0;
+	}
 	return 0;
 }
