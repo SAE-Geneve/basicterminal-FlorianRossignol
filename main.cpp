4a277ceba4422a8b97deea4c2c4883d09c55f27a
FlorianRossignol <rossignol.florian.fr@gmail.com>
2020-09-16 14:10:18 +0200

    attaque projet vaisseau

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
-#include <windows.h>
+#include <iostream>
 
-int main()
+int main ()
 {
-	MessageBox(nullptr, "World!", "Hello", MB_ICONEXCLAMATION);
+	std::cout<<"un vaisseau ennemi entre dans votre champ de vision\n\n";
+	std::cout << "combiens de d\x82\gats lui inflige vous ? (maximum 4)\n\n";
+	int pv = 2;
+	std::cin >> pv ,"\n\n";
+	if (pv <= 2)
+	{
+		std::cout << "perdu le vaisseau avait plus de pv \n\n";
+	}
+	if (pv >2)
+	{
+		std::cout << "gagn\x82\ le vaisseau est battu " "\n\n";
+	}
+	return 0;
 }
